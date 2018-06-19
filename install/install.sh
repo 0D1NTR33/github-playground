651b99c3d73601dea8690606a0ecf37ae4679f9a
Mx <38440668+MxShift@users.noreply.github.com>
2018-06-20 00:59:38 +0500

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -7,11 +7,19 @@
 # http://askubuntu.com/questions/760085/how-do-you-install-google-chrome-on-ubuntu-16-04
 
 # Versions.
+COLS="$(tput cols)"
 CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
 SELENIUM_STANDALONE_VERSION=3.4.0
 SELENIUM_SUBDIR=$(echo "$SELENIUM_STANDALONE_VERSION" | cut -d"." -f-2)
 
 # Remove existing downloads and binaries so we can start from scratch.
+SETUP="REMOVING EXISTING DOWNLOADS AND BINARIES"
+
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+printf "%."$[ ($COLS - ${#SETUP}) / 2 ]"d" 0 | sed s/0/" "/g
+printf "$SETUP"
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+
 sudo apt-get remove google-chrome-stable
 rm ~/selenium-server-standalone-*.jar
 rm ~/chromedriver_linux64.zip
@@ -19,16 +27,37 @@ sudo rm /usr/local/bin/chromedriver
 sudo rm /usr/local/bin/selenium-server-standalone.jar
 
 # Install dependencies.
+SETUP="INSTALLING DEPENDENCIES"
+
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+printf "%."$[ ($COLS - ${#SETUP}) / 2 ]"d" 0 | sed s/0/" "/g
+printf "$SETUP"
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+
 sudo apt-get update
 sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
 
 # Install Chrome.
+SETUP="INSTALLING CHROME"
+
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+printf "%."$[ ($COLS - ${#SETUP}) / 2 ]"d" 0 | sed s/0/" "/g
+printf "$SETUP"
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable
 
 # Install ChromeDriver.
+SETUP="INSTALLING CHROMEDRIVER"
+
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+printf "%."$[ ($COLS - ${#SETUP}) / 2 ]"d" 0 | sed s/0/" "/g
+printf "$SETUP"
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+
 wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
@@ -37,12 +66,26 @@ sudo chown root:root /usr/local/bin/chromedriver
 sudo chmod 0755 /usr/local/bin/chromedriver
 
 # Install Selenium Server.
+SETUP="INSTALLING SELENIUM SERVER"
+
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+printf "%."$[ ($COLS - ${#SETUP}) / 2 ]"d" 0 | sed s/0/" "/g
+printf "$SETUP"
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+
 wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar -P ~/
 sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
 sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
 sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
 
 # Install Python.
+SETUP="INSTALLING PYTHON"
+
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+printf "%."$[ ($COLS - ${#SETUP}) / 2 ]"d" 0 | sed s/0/" "/g
+printf "$SETUP"
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt autoremove
@@ -50,5 +93,12 @@ sudo apt-get install python3
 sudo apt-get install python3-pip
 
 # Install Selenium module.
+SETUP="INSTALLING SELENIUM MODULE"
+
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+printf "%."$[ ($COLS - ${#SETUP}) / 2 ]"d" 0 | sed s/0/" "/g
+printf "$SETUP"
+printf "\n%."$COLS"d" 0 | sed s/0/+/g
+
 sudo pip3 install --upgrade pip
 sudo pip3 install selenium
